2455bed0ea5b918f5b6f2c84eb64adce1264d981
Sarathi Prabu <sarathiprabum@gmail.com>
2024-04-28 21:40:00 -0400

    Updated main.py - Added customers endpoint

--- main.py
+++ main.py
@@ -1,23 +1,80 @@
-from http.client import HTTPException
-from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, HTTPException
+import sqlite3
+from pydantic import BaseModel
 
-DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+class Customer(BaseModel):
+    customer_id: int | None = None
+    name: str
+    phone: str
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message":"Hello World"}
-
 @app.get("/customers/{customer_id}")
 def read_customer(customer_id: int, q=None):
-    pass
+    conn = sqlite3.connect('db.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("SELECT customer_id, name, phone from customers where customer_id = ?", (customer_id,))
+    customer = cursor.fetchone()
+    conn.close()
+    if customer != None:
+        return Customer(customer_id=customer[0],name=customer[1],phone=customer[2])
+    else:
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+@app.post("/customers/", responses={409:{"detail":"Mobile Number already exists"}})
+def create_customer(customer:Customer):
+    try:
+        conn = sqlite3.connect('db.sqlite')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO CUSTOMERS (name, phone) VALUES(?,?)",(customer.name,customer.phone))
+        customer.customer_id = cursor.lastrowid
+        conn.commit()
+    except sqlite3.IntegrityError as e:
+        raise HTTPException(status_code=409, detail="Mobile Number already exists") 
+    finally:
+        conn.close()
+    return customer
+
+# Use a decorator to validate the customer id is passed
+@app.put("/customers/{customer_id}", responses={400:{"detail":"BAD REQUEST Customer ID is required!"},404: {"detail": "Customer not found! Update failed!!"}})
+def update_customer(customer_id: int,customer:Customer):
+    if customer.customer_id != customer_id:
+        raise HTTPException(status_code=400, detail="Invalid Customer ID!") 
+    try:
+        conn = sqlite3.connect('db.sqlite')
+        cursor = conn.cursor()
+        cursor.execute(
+            """UPDATE CUSTOMERS 
+            SET name = ?,
+            phone=? 
+            WHERE customer_id = ?""",
+            (customer.name,customer.phone, customer.customer_id))
+        modified = cursor.rowcount
+        if modified > 0:
+            conn.commit()
+            updated_customer = read_customer(customer.customer_id)
+            return updated_customer
+        else:
+            raise HTTPException(status_code=404, detail="Customer not found. Update failed!!") 
+    finally:
+        conn.close()
+    return customer
+
+@app.delete("/customers/{customer_id}",responses={404: {"detail": "Customer ID not found"}})
+def delete_customer(customer_id:int):
+    if customer_id == None:
+        raise HTTPException(status_code=400, detail="Invalid Customer ID!") 
+    try:
+        conn = sqlite3.connect('db.sqlite')
+        cursor = conn.cursor()
+        cursor.execute(
+            """DELETE FROM CUSTOMERS 
+            WHERE customer_id = ?""",
+            (customer_id,))
+        modified = cursor.rowcount
+        if modified > 0:
+            conn.commit()
+        else:
+            raise HTTPException(status_code=404, detail="Customer ID not found") 
+    finally:
+        conn.close()
+    return {"Message":f"{modified} rows affected"}
